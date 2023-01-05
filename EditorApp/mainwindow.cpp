1f560d2299fb10d76d7a38dae343022ff77e3212
zhangzhe1218 <zhangzhe1588zz@163.com>
2023-01-05 19:40:02 +0800

    <fix>([visualCurve && Ray picking]): <fix the bug of 0 frame show bug && fix the bug of update modelMatrix by modifying node property about ray picking>

--- EditorApp/mainwindow.cpp
+++ EditorApp/mainwindow.cpp
@@ -291,12 +291,7 @@ ads::CDockAreaWidget* createAndAddObjectTree(const char* title, const char* dock
 	QObject::connect(newTreeView, &raco::object_tree::view::ObjectTreeView::setTextureResHandles, mainWindow, &MainWindow::setTextureResHandles);
 	QObject::connect(newTreeView, &raco::object_tree::view::ObjectTreeView::updateNodeHandles, mainWindow, &MainWindow::updateNodeHandles);
 
-	QString tempTitle(title);
-	if (tempTitle.compare(QString("Scene Graph")) == 0) {
-        newTreeView->updateMeshData();
-		nodeDataPro->setNodeNameHandleReMap(newTreeView->updateNodeTree());
-		nodeDataPro->AnalyzeHandle();
-	}
+    QString tempTitle(title);
 	if (sortFilterModel) {
 		newTreeView->setSortingEnabled(true);
 		newTreeView->sortByColumn(raco::object_tree::model::ObjectTreeViewDefaultModel::COLUMNINDEX_TYPE, Qt::SortOrder::AscendingOrder);
@@ -311,7 +306,13 @@ ads::CDockAreaWidget* createAndAddObjectTree(const char* title, const char* dock
 
 	dockObjectView->setTreeView(newTreeView);
 	treeDockManager.addTreeDock(dockObjectView);
-	dockModel->setParent(dockObjectView);
+    dockModel->setParent(dockObjectView);
+
+    if (tempTitle.compare(QString("Scene Graph")) == 0) {
+        newTreeView->updateMeshData();
+        nodeDataPro->setNodeNameHandleReMap(newTreeView->updateNodeTree());
+        nodeDataPro->AnalyzeHandle();
+    }
 
 	dockObjectView->setObjectName(dockObjName);
 	return dockManager->addDockWidget(area, dockObjectView, dockArea);
